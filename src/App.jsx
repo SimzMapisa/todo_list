f5f787ff19e24b33f867ee8ee073901feee6f4d4
SimzMapisa <simbarashe795@gmail.com>
2023-06-09 15:20:23 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+	{/* 	const [count, setCount] = useState(0) */ }
+	return (
+		<>
+			<h1>Hello</h1>
+			<div>
+				<p>yes</p>
+				<h4>hey</h4>
+			</div>
+		</>
+	)
+}
+
+export default App
