b43272b5ed4d8c7b6d975106731961878ed8279e
SimzMapisa <simbarashe795@gmail.com>
2023-06-10 19:35:46 +0200

    add navbar component and bodycontent which holds all the other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import Navbar from './components/Navbar'
+import BodyContent from './components/BodyContent'
 
-function App() {
-	return (
-		<>
-			<h1>Hello</h1>
-		</>
-	)
+function App () {
+  return (
+    <div className='main'>
+      <img src='../src/assets/images/bg-desktop-dark.jpg' alt='' className='bg-img' />
+      <div className='wrapper'>
+        <Navbar />
+        <BodyContent />
+      </div>
+    </div>
+  )
 }
 
 export default App
