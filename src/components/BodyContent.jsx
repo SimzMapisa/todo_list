6c5d99ebf7975793ba8718b44165080921ae72bb
SimzMapisa <simbarashe795@gmail.com>
2023-06-17 12:26:08 +0200

    add icons to Todo and add styling classes to both BodyC, add remove and add functionality

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -1,11 +1,23 @@
-import InputBox from './InputBox'
+import { useContext } from 'react';
+import BottomNav from './BottomNav';
+import InputBox from './InputBox';
+import Todo from './Todo';
+import { TodoContext } from '../contexts/Todos';
 
 const BodyContent = () => {
-  return (
-    <div className='content'>
-      <InputBox />
-    </div>
-  )
-}
+	const { todos } = useContext(TodoContext);
 
-export default BodyContent
+	return (
+		<div className='main-wrapper'>
+			<InputBox className='drag' />
+			<div className='content'>
+				{todos.map((todo) => {
+					return <Todo key={todo.id} text={todo.text} id={todo.id} />;
+				})}
+				<BottomNav />
+			</div>
+		</div>
+	);
+};
+
+export default BodyContent;
