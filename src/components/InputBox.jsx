c21ed878c72ecc563bd23cf7b255a4b35135787b
SimzMapisa <simbarashe795@gmail.com>
2023-06-17 12:21:11 +0200

    add styling classes and turn auto complete off

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react';
+import { TodoContext } from '../contexts/Todos';
+import { v4 as uuidv4 } from 'uuid';
 
 const InputBox = () => {
-  const [inputVal, setInputval] = useState('')
+	const [inputVal, setInputval] = useState('');
+	const { addTodo } = useContext(TodoContext);
+	const handleChange = (e) => {
+		setInputval(e.target.value);
+	};
 
-  const handleChange = (e) => {
-    setInputval(e.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <input type='text' onChange={handleChange} name='todo' value={inputVal} placeholder='Hello ' />
-      </form>
-    </div>
-  )
-}
+	const newTodo = {
+		id: uuidv4(),
+		text: inputVal,
+		completed: false,
+	};
 
-export default InputBox
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addTodo(newTodo);
+		setInputval('');
+	};
+
+	return (
+		<div className='form-wrapper'>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					onChange={handleChange}
+					name='todo'
+					value={inputVal}
+					placeholder='Create a new todo'
+					autoComplete='off'
+				/>
+			</form>
+		</div>
+	);
+};
+
+export default InputBox;
